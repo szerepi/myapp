502a36be04c681a53d4f2cfbc30181d912a698aa
DIR\ildiko.szerepi <szerepi@yahoo.com>
2018-04-05 11:43:56 +0300

    Resolve warnings

--- src/components/Users/user.js
+++ src/components/Users/user.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
-import ReactTable from 'react-table'
-import "react-table/react-table.css";
 
 class User extends Component {
     render() {        
